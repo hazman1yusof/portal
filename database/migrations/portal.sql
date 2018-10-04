f4f3e233c77073952c606382549e55f7f3ad2a11
hazman1yusof <hazman.yusof@gmail.com>
2018-10-04 16:12:26 +0800

    sdsd

--- database/migrations/portal.sql
+++ database/migrations/portal.sql
@@ -1,8 +1,8 @@
 /*
-SQLyog Community v12.5.1 (64 bit)
-MySQL - 5.7.19 : Database - myportal
+SQLyog Community v12.2.2 (64 bit)
+MySQL - 10.2.3-MariaDB-log : Database - myportal
 *********************************************************************
-*/
+*/
 
 /*!40101 SET NAMES utf8 */;
 
@@ -22,20 +22,24 @@ DROP TABLE IF EXISTS `activity_detail`;
 
 CREATE TABLE `activity_detail` (
   `activity_date` date DEFAULT NULL,
-  `activity_name` text,
+  `activity_name` text DEFAULT NULL,
   `activity_time` varchar(10) DEFAULT NULL,
   `activity_venue` varchar(20) DEFAULT NULL,
   `activity_image` varchar(30) DEFAULT NULL,
-  `activity_desc` text
+  `activity_desc` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Table structure for table `carousel_img` */
+/*Table structure for table `carousel` */
 
-DROP TABLE IF EXISTS `carousel_img`;
+DROP TABLE IF EXISTS `carousel`;
 
-CREATE TABLE `carousel_img` (
-  `carousel_image` varchar(30) DEFAULT NULL,
-  `carousel_text` varchar(30) DEFAULT NULL
+CREATE TABLE `carousel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `lineno` int(5) DEFAULT NULL,
+  `carousel_path` varchar(30) DEFAULT NULL,
+  `carousel_text` varchar(30) DEFAULT NULL,
+  `active` varchar(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*Table structure for table `info_detail` */
@@ -45,7 +49,7 @@ DROP TABLE IF EXISTS `info_detail`;
 CREATE TABLE `info_detail` (
   `info_name` varchar(30) DEFAULT NULL,
   `info_date` date DEFAULT NULL,
-  `info_content` text,
+  `info_content` text DEFAULT NULL,
   `info_image` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -82,7 +86,7 @@ DROP TABLE IF EXISTS `module_master`;
 CREATE TABLE `module_master` (
   `module_image` varchar(30) DEFAULT NULL,
   `module_text` varchar(30) DEFAULT NULL,
-  `module_detail` text
+  `module_detail` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
